a0a0d99a079cb8d13dc3c50865354b26912b69a7
Rohit Pandey <56378143+rohit-pandey96@users.noreply.github.com>
2020-06-15 04:10:07 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('CORS');
+const cors = require('cors');
 const routes = require('./routes/routing');
 const body_parser = require('body-parser');
 const port = process.env.PORT || 3000;
